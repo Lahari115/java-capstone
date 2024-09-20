2782c99bda0ace58ca1467180f55da26779b7b77
Lahari115 <124115199+Lahari115@users.noreply.github.com>
2024-09-20 10:53:53 +0530

    Create script.js

---
+++ script.js
@@ -0,0 +1,5 @@
+function handleRegistration(event) {
+    event.preventDefault(); // Prevent the form from submitting normally
+    // Redirect to the thank you page
+    location.href = 'thank-you.html';
+}
